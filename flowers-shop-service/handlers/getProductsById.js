8ef651c7a59bb7dd80bdc5228512ee9727bb5b71
d.novak <d.novak@eisgroup.com>
2023-02-27 21:49:18 +0100

    3

--- flowers-shop-service/handlers/getProductsById.js
+++ flowers-shop-service/handlers/getProductsById.js
@@ -1,33 +1,45 @@
 const products = [
   {
-    "product":
-      { "description": "Red Roses Bouquet", "id": "8b6d64e9-7d50-4c6e-80b4-ec7044c4a4a1", "price": 50, "title": "Red Roses" }
-    , "count": 1
+      "description": "Short Product Description1",
+      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+      "price": 24,
+      "title": "ProductOne",
+      "count": 1
   },
   {
-    "product":
-      { "description": "Mixed Color Tulips", "id": "44e142e3-6e26-4dd5-b6c8-6a0f0dcb580a", "price": 25, "title": "Tulip Bouquet" }
-    , "count": 3
+      "description": "Short Product Description7",
+      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+      "price": 15,
+      "title": "ProductTitle",
+      "count": 2
   },
   {
-    "product":
-      { "description": "Pink and White Lilies", "id": "e8a843a8-9a9f-4840-bbfa-40f04e0f7e11", "price": 35, "title": "Lily Bouquet" },
-    "count": 2
+      "description": "Short Product Description2",
+      "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+      "price": 23,
+      "title": "Product",
+      "count": 3
   },
   {
-    "product":
-      { "description": "Pink and White Lilies", "id": "e8a843a8-9a9f-4840-bbfa-40f04e0f7e11", "price": 35, "title": "Lily Bouquet" },
-    "count": 5
+      "description": "Short Product Description4",
+      "id": "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+      "price": 15,
+      "title": "ProductTest",
+      "count": 4
   },
   {
-    "product":
-      { "description": "Pink and White Lilies", "id": "e8a843a8-9a9f-4840-bbfa-40f04e0f7e11", "price": 35, "title": "Lily Bouquet" },
-    "count": 10
+      "description": "Short Product Descriptio1",
+      "id": "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+      "price": 23,
+      "title": "Product2",
+      "count": 5
   },
   {
-    "product":
-      { "description": "Pink and White Lilies", "id": "e8a843a8-9a9f-4840-bbfa-40f04e0f7e11", "price": 35, "title": "Lily Bouquet" },
-    "count": 11
+      "description": "Short Product Description7",
+      "id": "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+      "price": 15,
+      "title": "ProductName",
+      "count": 6
   }
 ]
   
@@ -44,6 +56,10 @@ const products = [
     }
   
     return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       statusCode: 200,
       body: JSON.stringify(product),
     };
